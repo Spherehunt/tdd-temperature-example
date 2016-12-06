30402356957cb0fe944a81b51190d7504a94e988
Jonas Demeulemeester <jonasdemeulemeester@hotmail.be>
2016-12-06 16:02:20 +0100

    First test successful with hardcoded temperature value

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -1,17 +1,23 @@
 
 class Temperature
-    attr_accessor :temperature
+  attr_accessor :temperature
 
-    FAHRENHEIT_OFFSET = 32
-    FAHRENHEIT_SLOPE = Rational(9,5)
+  FAHRENHEIT_OFFSET = 32
+  FAHRENHEIT_SLOPE = Rational(9,5)
+  KELVIN = 273.15
 
-    def initialize temperature
-        throw Exception unless temperature.kind_of? Numeric
-        @temperature = temperature
-    end
+  def initialize temperature
+    throw Exception unless temperature.kind_of? Numeric
+    @temperature = temperature
+  end
 
-    def to_fahrenheit
-        FAHRENHEIT_OFFSET + temperature * FAHRENHEIT_SLOPE
-    end
+  def to_fahrenheit
+    FAHRENHEIT_OFFSET + temperature * FAHRENHEIT_SLOPE
+  end
+
+  def to_kelvin
+    273.15
+    # temperature + KELVIN
+  end
 
 end
